946cd1e4ca13430a4a936ff2b25b665f73800a66
khalidaazizi <khalidaazizi092@gmail.com>
2025-10-14 14:08:32 +0430

    add features

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -15,7 +15,10 @@ function Layout({ children }: ILayout) {
     show: boolean;
   } | null>(null);
 
- 
+  const _showToast = (productName: string, type: "cart" | "wishlist" | "compare") => {
+    setToastData({ productName, type, show: true });
+    setTimeout(() => setToastData(null), 4000); // خودکار مخفی شود
+  };
 
   const closeToast = () => setToastData(null);
 
