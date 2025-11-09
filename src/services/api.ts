cfeba9e71a702d536f65ef55702407f86a21e4c1
khalidaazizi <khalidaazizi092@gmail.com>
2025-11-09 17:18:40 +0430

    Use my-json-server for products

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const client = axios.create({
-    baseURL:"http://localhost:3000"
+    // baseURL:"http://localhost:3000"
+    baseURL:"https://my-json-server.typicode.com/khalidaazizi/fake-api"
 })
 export async function getProducts() {
     const {data} = await client("/products")
