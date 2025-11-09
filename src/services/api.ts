755a7ec7c0191b52515c49cb4053de5b2934729f
khalidaazizi <khalidaazizi092@gmail.com>
2025-11-09 17:49:27 +0430

    Use my-json-server for products

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,33 +1,19 @@
-// import axios from "axios";
-
-// const client = axios.create({
-//     // baseURL:"http://localhost:3000"
-//     baseURL:"https://my-json-server.typicode.com/khalidaazizi/fake-api"
-// })
-// export async function getProducts() {
-//     const {data} = await client("/products")
-//     return data
-    
-// }
-// export async function getProduct(id:number | string) {
-//     const {data} = await client(`/products/${id}`)
-//     return data
-
-
-// }
-
 import axios from "axios";
 
 const client = axios.create({
-  baseURL: "https://raw.githubusercontent.com/khalidaazizi/fake-api/main"
-});
-
+    // baseURL:"http://localhost:3000"
+    baseURL:"https://my-json-server.typicode.com/khalidaazizi/fake-api"
+})
 export async function getProducts() {
-  const { data } = await client.get("/db.json");
-  return data.products; // آرایه products را جدا می‌کنیم
+    const {data} = await client("/products")
+    return data
+    
+}
+export async function getProduct(id:number | string) {
+    const {data} = await client(`/products/${id}`)
+    return data
+
+
 }
 
-export async function getProduct(id: number | string) {
-  const { data } = await client.get("/db.json");
-  return data.products.find((p: any) => p.id === Number(id));
-}
+
